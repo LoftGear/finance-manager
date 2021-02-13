489f29bf26dc8cdfaee59dd4f21f8f362b1a3bd3
Maxim Baginskiy <kachan1208@gmail.com>
2021-02-13 22:22:22 +0200

    init

---
+++ lib/screens/expenses_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExpensesManager extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expenses"),
+      ),
+      body: Text("Hello world"),
+    );
+  }
+}
